92fa1d7c1e1b3ef69dd7cfe7b3a88f56fea1d165
theJonathanXue <80607439+theJonathanXue@users.noreply.github.com>
2024-07-15 10:17:09 -0400

    Import db functions

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,4 +1,8 @@
 import { auth, signOut } from 'app/auth';
+import { getAllMangaList } from 'app/db';
+import { getAllUserMangaList } from 'app/db';
+import { deleteUserMangaByTitle } from 'app/db';
+import { insertUserManga } from 'app/db';
 
 export default async function ProtectedPage() {
   let session = await auth();
@@ -7,6 +11,7 @@ export default async function ProtectedPage() {
     <div className="flex h-screen bg-black">
       <div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center text-white">
         You are logged in as {session?.user?.email}
+        Your id {session?.user?.id}
         <SignOut />
       </div>
     </div>
